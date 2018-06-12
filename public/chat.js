9a71713d907abe3e0d732614f162fcd17fce8e39
butachin <b1016094@fun.ac.jp>
2018-06-12 13:03:36 +0900

    title

--- public/chat.js
+++ public/chat.js
@@ -37,13 +37,11 @@ messagesRef.limitToLast(10).on('child_added', function (snapshot) {
     var data = snapshot.val();
     var username = data.name || "anonymous";
     var message = data.text;
-    var time = new Data();
-    messagetime = (time.getFullYear() + "/" +  time.getMonth() + 1 + "/"+ time.getDate()  + "/" + time.getHours() + "/" + time.getMinutes());
 
     //取得したデータの名前が自分の名前なら右側に吹き出しを出す
     if ( username == nameField.val() ) {
 
-        var messageElement = $("<il><p class='sender_name me'>" + username + "</p><p class='right_balloon'>" + message + "</p><p class='clear_balloon'></p>/il>");
+        var messageElement = $("<il><p class='sender_name me'>" + username + "</p><p class='right_balloon'>" + message + "</p><p class='clear_balloon'></p></il>");
 
     } else {
 
